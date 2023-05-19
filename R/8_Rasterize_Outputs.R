5ddce8c70406cfbf95ab67ca37048ca984d23f18
Francoisdt14 <fdutoit2009@gmail.com>
2023-05-19 15:05:17 -0700

    29-05-2023
    Differences between NTEMS & 3PG

--- R/8_Rasterize_Outputs.R
+++ R/8_Rasterize_Outputs.R
@@ -163,18 +163,15 @@ volume_canada <- rast("Y:/Muise/francois4/Study_Area_M_nineS/mosaiced/structure/
 biomass_canada <- rast("Y:/Muise/francois4/Study_Area_M_nineS/mosaiced/structure/total_biomass/total_biomass_2021.dat")
 
 ##################
-
 # Set the projection of the large raster to match the small raster
-biomass_canada <- project(biomass_canada, crs(group.of.49))
+volume_canada <- project(volume_canada, crs(volume_3pg))
 
 # Crop the masked large raster to the extent of the small raster
-biomass_canada_cropped <- crop(basal_canada, group.of.49)
+volume_canada_cropped <- crop(volume_canada, volume_3pg)
 # Write the raster
-writeRaster(biomass_canada_cropped, filename = paste0("D:/BP_Layers/outputs/crops/ntems_rasters/","biomass", "_","49",".tif"))
+# writeRaster(volume_canada_cropped, filename = paste0("D:/BP_Layers/outputs/crops/ntems_rasters/","volume", "_","49",".tif"))
 # Check that it displays correctly
 plot(volume_canada_cropped, col=viridis(100),  main="NTEMS")
-
-
 
 
 ##################### DIFFERENCE OUR OUTPUT WITH NTEMS
@@ -187,6 +184,9 @@ basal_ntems <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/basal_49.tif")
 biomas_ntems <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/biomass_49.tif")
 volume_ntems <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/volume_49.tif")
 
+basal_ntems <- (basal_ntems / 100)
+biomas_ntems <- (biomas_ntems / 100)
+volume_ntems <- (volume_ntems / 10)
 
 diff_basal <- (basal_3pg - basal_ntems)
 diff_biomass <- (biomas_3pg - biomas_ntems)
@@ -195,24 +195,85 @@ diff_volumes <- (volume_3pg - volume_ntems)
 # Set up the plot window to have three columns
 par(mfrow = c(1, 3))
 
-plot(basal_3pg, col=viridis(100),  main="3PG")
-plot(basal_ntems, col=viridis(100),  main="NTEMS")
+plot(basal_3pg, col=viridis(100),  main="3PG basal")
+plot(basal_ntems, col=viridis(100),  main="NTEMS basal")
 plot(diff_basal, col=viridis(100),  main="DIFFERENCE")
+
+plot(biomas_3pg, col=viridis(100),  main="3PG biomass")
+plot(biomas_ntems, col=viridis(100),  main="NTEMS biomass")
+plot(diff_biomass, col=viridis(100),  main="DIFFERENCE")
+
+plot(volume_3pg, col=viridis(100),  main="3PG volume")
+plot(volume_ntems, col=viridis(100),  main="NTEMS volume")
+plot(diff_volumes, col=viridis(100),  main="DIFFERENCE")
 
 # Reset the par to the default of one plot per window
 par(mfrow = c(1, 1))
 
-# WORTHWHILE TO NORMALIZE EVERYTHING TO 1 TO LOOK FOR SPATIAL PATTERNS?
+# WORTHWHILE TO NORMALIZE EVERYTHING TO 1 TO LOOK FOR SPATIAL PATTERNS??
 
+rast1 <- volume_ntems
 # Get the minimum and maximum values of the raster
-min_value <- minmax(basal_3pg)[1,]
-max_value <- minmax(basal_3pg)[2,]
+min_value <- minmax(rast1)[1,]
+max_value <- minmax(rast1)[2,]
 
 # Rescale the raster between 0 and 1
-basal_3pg_NORM <- (basal_3pg - min_value) / (max_value - min_value)
+rast_NORM <- (rast1 - min_value) / (max_value - min_value)
+plot(rast_NORM, col=viridis(100),  main="volume_ntems Normalized")
 
-plot(basal_3pg_NORM, col=viridis(100),  main="3PG Normalized")
 
+writeRaster(rast_NORM, filename = "D:/BP_Layers/outputs/crops/ntems_rasters/volume_ntems_NORM.tif")
+
+######
+basal_3pg_NORM <- rast("D:/BP_Layers/outputs/crops/output_rasters/49_composite/basal_3pg_NORM.tif")
+biomass_3pg_NORM <- rast("D:/BP_Layers/outputs/crops/output_rasters/49_composite/biomass_3pg_NORM.tif")
+volume_3pg_NORM <- rast("D:/BP_Layers/outputs/crops/output_rasters/49_composite/volume_3pg_NORM.tif")
+
+basal_ntems_NORM <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/basal_ntems_NORM.tif")
+biomass_ntems_NORM <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/biomas_ntems_NORM.tif")
+volume_ntems_NORM <- rast("D:/BP_Layers/outputs/crops/ntems_rasters/volume_ntems_NORM.tif")
+
+
+norm_diff_basal <- (basal_3pg_NORM - basal_ntems_NORM)
+norm_diff_biomass <- (biomass_3pg_NORM - biomass_ntems_NORM)
+norm_diff_volume <- (volume_3pg_NORM - volume_ntems_NORM)
+
+# Set up the plot window to have three columns
+par(mfrow = c(1, 3))
+
+plot(basal_3pg_NORM, col=viridis(100),  main="Normalized 3PG basal")
+plot(basal_ntems_NORM, col=viridis(100),  main="Normalized NTEMS basal")
+plot(norm_diff_basal, col=viridis(100),  main="DIFFERENCE")
+
+
+diff <- norm_diff_basal
+
+# Define the thresholds and colors
+thresh_red <- 0.7
+thresh_yellow <- 0.3
+
+color_red <- "red"
+color_yellow <- "blue"
+color_green <- "green"
+
+# Reclassify the raster
+reclassified_raster <- diff
+# Reclassify the raster
+reclassified_raster <- reclassify(diff,
+                                  cbind(-Inf, -thresh_red, NA,
+                                        -thresh_red, thresh_red, NA,
+                                        thresh_red, Inf, NA,
+                                        -thresh_yellow, thresh_yellow, 1,
+                                        -thresh_red, thresh_red, 2,
+                                        thresh_yellow, thresh_red, 2,
+                                        -thresh_red, -thresh_yellow, 3,
+                                        -thresh_red, thresh_red, 3))
+
+# Define the color palette
+color_palette <- c(color_green, color_yellow, color_red)
+
+# Plot the reclassified raster
+plot(reclassified_raster, col = color_palette)
 ############################## LOOK AT SUMMARY STATISTICS OF THE DATA
 # Convert raster to data frame
 df <- as.data.frame(volume_canada_cropped, xy = TRUE)
